840644637d50ec5b1e980c0a6693e4c65ead2982
HARSH SAHU <127614860+Harshsahu11@users.noreply.github.com>
2025-07-04 16:27:03 +0530

    Update FirebaseConfig

--- src/fireabase/FirebaseConfig.jsx
+++ src/fireabase/FirebaseConfig.jsx
@@ -4,12 +4,12 @@ import {getAuth} from 'firebase/auth';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAckNST9QwPP20f2c6R24GqGzhV7sp5FUU",
-  authDomain: "myfirstproject-dafbc.firebaseapp.com",
-  projectId: "myfirstproject-dafbc",
-  storageBucket: "myfirstproject-dafbc.firebasestorage.app",
-  messagingSenderId: "70298968396",
-  appId: "1:70298968396:web:6a03033c9fdc4046966fe9"
+  apiKey:
+  authDomain: 
+  projectId: 
+  storageBucket: 
+  messagingSenderId: 
+  appId:
 };
 
 const app = initializeApp(firebaseConfig);
